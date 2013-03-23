904cff797fea625637cde6f9dcaf418f9c6855f9
NHQ <jonathon@nationalheadquarters.org>
2013-03-22 18:58:47 -0700

    init

---
+++ index.js
@@ -0,0 +1,84 @@
+var parse = require('midi-file-parser');
+
+module.exports = function(sampleRate, file){
+
+	try{
+		var midi = parse(file);
+	}
+	catch(err){throw new Error('Error parsing Midi File')}
+	
+	var header = midi.header, tracks = midi.tracks;
+	
+	if(header.formatType > 1){
+		throw new Error('Midi format ' + header.formatType + ' is not supported.')
+	};
+	
+	sampleRate = sampleRate || 44100
+	
+	var bpm = msPerBeat = ticksPerBeat = tps = samplesPerTick = TIME = 0
+
+	var tpb = header.ticksPerBeat
+	
+	function initClock(BPM){
+		bpm = BPM || 120
+		msPerBeat = (bpm / 60) * 1e6
+		tps = (bpm * tpb) / 60
+		samplesPerTick = sampleRate / tps
+	}
+	
+	initClock();
+	
+	var TRACKS = [];
+	
+	for(x in tracks){ // get absolute times for events
+		var track = tracks[x];
+		var absolute = 0
+		var t = track.map(function(event){
+			event.absolute = absolute += event.deltaTime;
+			return event
+		});
+		TRACKS.push(t)
+	};
+	
+	var ticks, events = [];
+	
+	console.log(TRACKS.length)
+
+	return function(time, trackNum){ // pass optional trackNum to return events for only that track
+		
+		ticks = time * tps;
+				
+		events.splice(0);
+		
+		for(x in TRACKS){ 
+			var track = TRACKS[x], shift = 0;
+			for(e in track){
+				var event = track[e];
+				if (event.absolute <= ticks){
+					if(!(event.type == 'channel')){						
+						shift++
+						switch(event.subtype){
+							case 'setTempo':
+  							var bpm = 60000000 / event.microsecondsPerBeat;
+	  						initClock(bpm);
+								break;
+						}
+					}
+					else{
+						shift++
+						events.push(event);	
+					}
+				}
+				else {
+					break;
+				}
+			};
+			for(; shift; shift--){
+				track.shift()
+			}
+		}
+	  
+		return events		
+	
+	}
+}
---
+++ test.js
@@ -0,0 +1,34 @@
+var player = require('./index');
+var baudio = require('../baudio');
+var b = baudio({rate:8000, size: 256});
+var fs = require('fs');
+file = fs.readFileSync('./hardmony.mid', 'binary');
+midi = player(44100, file);
+notes = {};
+tau = Math.PI * 2;
+
+b.push(function(time){
+	var e = midi(time);
+	if(e.length){
+		e.forEach(function(event){
+			if(event.subtype == 'noteOn'){
+				var f = 440 * Math.pow(2, (event.noteNumber - 49) / 12);
+//			console.log(event.channel, f)
+				notes[f] = {play: true, start: new Date().getTime()}
+			}
+			if(event.subtype == 'noteOff'){
+				var f = 440 * Math.pow(2, (event.noteNumber - 49) / 12);
+				notes[f] = {play: false}
+			}
+		})
+	};
+	
+	var sample = 0;
+	for(n in notes){
+		if(notes[n].play) sample += Math.sin(tau * time * Number(n))
+		else if((new Date().getTime() - notes[n].start) < 1 / 8) sample += Math.sin(tau * time * Number(n))
+	}
+	
+	return sample
+})
+b.play();
